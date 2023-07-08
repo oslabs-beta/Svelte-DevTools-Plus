bffbd2e98c3b881414c9e5ea3dfbb4fdcdafbcf6
Alex Vranas <avranas42@gmail.com>
2023-07-08 14:48:01 -0400

    Basic component rendering with mock data

--- devtool/src/pages/Panel/Panel.tsx
+++ devtool/src/pages/Panel/Panel.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Panel.css';
 import SplitPane from 'react-split-pane';
-
+import mockData from './mock-components.json';
+import TreeComponent from './PanelComponents/TreeComponent';
 const breakPoint = 50;
 
 function Panel() {
-  const spProps = {
-    split: 'vertical',
-  };
+
+  function logMockData() : React.ReactElement {
+    console.log(mockData)
+    return (<TreeComponent 
+      component={mockData.component}
+      children={mockData.children}
+      props={mockData.props}
+      state={mockData.state}
+      level={0}
+      />);
+  }
   return (
     <div className="container">
       <div id="content">
@@ -21,6 +30,7 @@ function Panel() {
         >
           <div className='pane'>
             <h1>Components</h1>
+            {logMockData()}
           </div>
           <div className='pane'>
             <h2>State</h2>
---
+++ devtool/src/pages/Panel/PanelComponents/TreeComponent.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './TreeComponent.css';
+import { v4 as uuidv4 } from 'uuid';
+
+interface TreeComponentProps {
+  component: string;
+  state: Object | null;
+  props: Object | null;
+  children: Array<any> | null;
+  level: number;
+}
+
+const TreeComponent: React.FC<TreeComponentProps> = ({
+  component,
+  state,
+  props,
+  children,
+  level,
+}: TreeComponentProps) => {
+  const childrenState: Array<JSX.Element> = [];
+  if (children) {
+    children.forEach((i) => {
+      childrenState.push(
+        <TreeComponent
+          component={i.component}
+          state={i.state}
+          props={i.props}
+          children={i.children}
+          level={level + 1}
+          key={uuidv4()}
+        />
+      );
+    });
+  }
+
+  const [focused, setFocused] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  function handleClick() {
+    setOpen( open ? false : true);
+  }
+
+  let componentString = '';
+  // for (let i = 0; i < level; i++) {
+  //   stringToDisplay = stringToDisplay.concat('....');
+  // }
+  componentString = componentString.concat('<', component, ' />');
+
+  return (
+    <div className="tree-component">
+      <p style={{paddingLeft: `${level}rem`}}onClick={handleClick}>{componentString}</p>
+      {/* <p>Component state: {JSON.stringify(state)}</p>
+    <p>Component props: {JSON.stringify(props)}</p> */}
+      {/* <p>Children:</p> */}
+      
+      {open ? childrenState.map((item, index) => {
+        console.log('item', item);
+        return item;
+      }) : null}
+    </div>
+  );
+};
+
+export default TreeComponent;
