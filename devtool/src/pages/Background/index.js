d97a90f13beeaa38dedb8408f01c12ec2dc58a39
Alex Vranas <avranas42@gmail.com>
2023-07-13 13:51:37 -0400

    Add functionality for getting Svelte Components

--- devtool/src/pages/Background/index.js
+++ devtool/src/pages/Background/index.js
@@ -1,22 +1,13 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+console.log('updated')
 
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('panel add listener');
-});
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('sender.tab', sender.tab);
-  console.log('Sending a response BACKGROUND');
-  sendResponse({ farewell: 'This is my response' });
-});
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.condition === false) {
-    // Set the specific 32x32 icon
-    chrome.action.setIcon({ path: { 32: 'icon-gray-34.png' } });
-  } else {
-    // Set the default 32x32 icon
-    chrome.action.setIcon({ path: { 32: 'icon-34.png' } });
-  }
-});
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   if (message.condition === false) {
+//     // Set the specific 32x32 icon
+//     chrome.action.setIcon({ path: { 32: 'icon-gray-34.png' } });
+//   } else {
+//     // Set the default 32x32 icon
+//     chrome.action.setIcon({ path: { 32: 'icon-34.png' } });
+//   }
+// });
