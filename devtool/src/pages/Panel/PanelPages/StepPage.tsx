ed616b7743309a15646a4241cc14d0a3bc5234fa
Alex Vranas <avranas42@gmail.com>
2023-07-17 14:35:08 -0400

    Give tree page access to root component data

--- devtool/src/pages/Panel/PanelPages/StepPage.tsx
+++ devtool/src/pages/Panel/PanelPages/StepPage.tsx
@@ -1,55 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../Panel.css";
 import TreeComponent from "../PanelComponents/TreeComponent";
+import { ComponentPageProps } from "../Panel";
 
-export default function StepPage() {
-  const [rootComponent, setRootComponent] = useState(<div></div>);
-
-  useEffect(() => {
-    // Sends a message to ContentScriptIsolated, telling it to get the
-    // current tab's root component
-    async function getRootComponent() {
-      // Get the tab the user is on
-      const [tab] = await chrome.tabs.query({
-        active: true,
-        lastFocusedWindow: true,
-      });
-      chrome.tabs.sendMessage(tab.id!, { message: "getRootComponent" });
-    }
-    getRootComponent();
-  }, []);
-
-  // Listen for response from ContentScriptIsolated. This is where we
-  // get the current tab's root Component, and update StepPage's state
-  chrome.runtime.onMessage.addListener(function (
-    message,
-    sender,
-    sendResponse
-  ) {
-    if (message.type === "returnRootComponent") {
-      const rootComponent = message.rootComponent;
-      if (message.rootComponent) {
-        setRootComponent(
-          <TreeComponent
-            component={rootComponent.component}
-            children={rootComponent.children}
-            componentProps={rootComponent.componentProps}
-            componentState={rootComponent.componentState}
-            level={0}
-          />
-        );
-      } else {
-        console.log("Error getting root component");
-      }
-    }
-  });
-
+const StepPage: React.FC<ComponentPageProps> = ({
+  rootComponentData,
+}: ComponentPageProps) => {
   return (
     <div className="pane">
       <>
-        <h1>Components</h1>
-        {rootComponent}
+        <h1>Component Step Page</h1>
+        {rootComponentData && (
+          <TreeComponent
+            component={rootComponentData.component}
+            children={rootComponentData.children}
+            componentProps={rootComponentData.componentProps}
+            componentState={rootComponentData.componentState}
+          />
+        )}
       </>
     </div>
   );
-}
+};
+
+export default StepPage;
