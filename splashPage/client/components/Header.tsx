b4ac81690873f771b371895f79532ae3005e29b1
francis8933 <francisespinoza33@gmail.com>
2023-07-17 11:01:57 -0400

    working on login and signup pages

--- splashPage/client/components/Header.tsx
+++ splashPage/client/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
       <img src={logo} width="80" height="80" alt="Svelte Logo" />
       <h1>SvelteTool Plus</h1>
       <p>Vesion 1.0</p>
-      <button>
+      <button className="downloadButton">
         <a href="https://www.w3schools.com/">Donwload</a>
       </button>
     </div>
