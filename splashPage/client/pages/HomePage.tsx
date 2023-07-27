3114b1d4e237da8d1aae2560ec174053768956e1
maciekmalecki <mathumalecki@Mathus-MacBook-Pro.local>
2023-07-27 16:40:47 -0400

    splash page update

--- splashPage/client/pages/HomePage.tsx
+++ splashPage/client/pages/HomePage.tsx
@@ -64,14 +64,14 @@ const HomePage = () => {
           },
           particles: {
             color: {
-              value: '(255, 62, 0, 0.2)',
+              value: '(255, 62, 0, 1)',
             },
             links: {
               color: '#ff3e00',
               distance: 150,
               enable: true,
-              opacity: 0.2,
-              width: 1,
+              opacity: 0.5,
+              width: 2,
             },
             move: {
               direction: 'none',
@@ -80,18 +80,18 @@ const HomePage = () => {
                 default: 'bounce',
               },
               random: false,
-              speed: 1,
+              speed: 0.5,
               straight: false,
             },
             number: {
               density: {
                 enable: true,
-                area: 800,
+                area: 1200,
               },
               value: 80,
             },
             opacity: {
-              value: 0.5,
+              value: 1,
             },
             shape: {
               type: 'circle',
