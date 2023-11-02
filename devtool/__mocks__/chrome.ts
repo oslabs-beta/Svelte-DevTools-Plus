1d697e179c3271fcfa1f4de2087b51bf161bd5cc
Alex Vranas <avranas42@gmail.com>
2023-11-01 18:46:14 -0700

    State injection updates mock data

--- devtool/__mocks__/chrome.ts
+++ devtool/__mocks__/chrome.ts
@@ -1,5 +1,6 @@
 import { Snapshot } from '../src/pages/Panel/slices/currentSnapshotSlice';
-import * as mockData from '../src/pages/Panel/mock-components.json';
+import data from './mockData';
+
 type ChromeMessageListener = (message: any) => void;
 
 interface MockRuntime {
@@ -30,7 +31,7 @@ interface MockMessageType {
 }
 
 interface MockTabs {
-  query: (queryInfo: any) => MockTabReturn[];
+  query: (queryInfo: any) => any[];
   sendMessage: (tabId: number, message: MockMessageType) => void;
 }
 
@@ -41,27 +42,52 @@ export interface MockChrome {
 
 const listeners: ChromeMessageListener[] = [];
 
+function updateState(id: number | undefined, newState: any): Boolean {
+  function helper(component: any): Boolean {
+    if (component.id === id) {
+      for (let i = 0; i < component.detail.ctx.length; i++) {
+        const state = component.detail.ctx[i];
+        if (state.key === Object.keys(newState)[0]) {
+          const value = Object.values(newState)[0];
+          if (state.value === value) return false;
+          state.value = value;
+          return true;
+        }
+      }
+      return false;
+    }
+    for (let i = 0; i < component.children.length; i++) {
+      const child = component.children[i];
+      return helper(child);
+    }
+    return false;
+  }
+  return helper(data);
+}
+
 const chrome: MockChrome = {
   runtime: {
     onMessage: {
-      addListener: jest.fn((callback) => {
+      addListener: (callback) => {
         listeners.push(callback);
-      }),
-      _triggerMessage: jest.fn((message) => {
+      },
+      _triggerMessage: (message) => {
         listeners.forEach((callback) => callback(message));
-      }),
+      },
     },
     sendMessage: function (message) {},
   },
   tabs: {
-    query: jest.fn().mockReturnValue([{ id: 0 }]),
-    sendMessage: jest.fn((tabId, request) => {
+    query: () => {
+      return [{ id: 0 }];
+    },
+    sendMessage: (tabId, request) => {
       switch (request.message) {
         case 'getRootComponent':
           {
             const message = {
               type: 'returnRootComponent',
-              rootComponent: mockData,
+              rootComponent: JSON.parse(JSON.stringify(data)),
             };
             listeners.forEach((f) => f(message));
           }
@@ -76,6 +102,16 @@ const chrome: MockChrome = {
           break;
         case 'injectState':
           {
+            const stateUpdated = updateState(
+              request.componentId,
+              request.newState
+            );
+            if (!stateUpdated) return;
+            const message = {
+              type: 'updateRootComponent',
+              rootComponent: JSON.parse(JSON.stringify(data)),
+            };
+            listeners.forEach((f) => f(message));
           }
           break;
         case 'injectSnapshot':
@@ -83,7 +119,7 @@ const chrome: MockChrome = {
           }
           break;
       }
-    }),
+    },
   },
 };
 
