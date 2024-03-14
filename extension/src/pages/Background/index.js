7458b648fe9e2542fda9b35a290d415acfb6aa1d
Alex Vranas <avranas42@gmail.com>
2024-03-14 11:16:54 -0700

    Add color to icon when Svelte app is being debugged

--- extension/src/pages/Background/index.js
+++ extension/src/pages/Background/index.js
@@ -1,20 +1,52 @@
-// console.log('This is the background page Put the background scripts here.');
+console.log('This is the background page Put the background scripts here.');
+
 // Send a message to the content script to let it know that
 // the devtool is closed
-console.log(chrome.runtime.getURL('grey-icon-34.png'));
-console.log('background script')
-chrome.action.setIcon({
-  path: {
-    "34": "grey-icon-34.png",
-    "128": "grey-icon-128.png"
-  }
-}, function() {
-  if (chrome.runtime.lastError) {
-    console.log("Error setting icon: " + chrome.runtime.lastError.message);
+function greyOutIcon() {
+  chrome.action.setIcon({
+    path: {
+      "34": "grey-icon-34.png",
+      "128": "grey-icon-128.png"
+    }
+  }, function() {
+    if (chrome.runtime.lastError) {
+      console.log("Error setting icon: " + chrome.runtime.lastError.message);
+    }
+  });
+}
+
+async function updateIcon(activeInfo) {
+  greyOutIcon();
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  chrome.tabs.sendMessage(tab.id, { message: 'getSvelteVersion' });
+}
+chrome.tabs.onUpdated.addListener(updateIcon);
+chrome.tabs.onActivated.addListener(updateIcon);
+
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.type === 'returnSvelteVersion') {
+    // If message.svelteVersion is null, the app is not using Svelte
+    if (message.svelteVersion) {
+      chrome.action.setIcon({
+        path: {
+          "34": "icon-34.png",
+          "128": "icon-128.png"
+        }
+      }, function() {
+        if (chrome.runtime.lastError) {
+          console.log("Error setting icon: " + chrome.runtime.lastError.message);
+        }
+      });
+    }
   }
 });
 
+
 chrome.runtime.onMessage.addListener(function (request) {
-  // The following is your async call
   chrome.runtime.sendMessage({ message: 'handleClosedPanel' });
 });
+
+greyOutIcon();
