70bf32ecd39456032cb7409869901f5324a13624
Alex Vranas <avranas42@gmail.com>
2024-09-04 10:11:49 -0700

    Add highlighted component store

---
+++ extension/src/pages/SveltePanel/src/panel/src/Store/highlightedComponent.ts
@@ -0,0 +1,14 @@
+  import { highlightedComponent } from './store';
+  import type { Component } from '../types';
+
+  export function setHighlightedComponent(payload: Component) {
+    highlightedComponent.set(payload);
+  }
+
+  export function updateHighlightedComponent(payload: Component) {
+    highlightedComponent.update(current => ({
+      ...current,
+      detail: payload.detail,
+      tagName: payload.tagName
+    }));
+  }
